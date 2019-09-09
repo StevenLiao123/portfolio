368e0c6e8b70371c763d6cb6df068924b35ceade
XIAOHUI LIAO <182272546@qq.com>
2019-09-09 20:52:04 +1000

    Added the type animation

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LandingPage.css';
+import Typist from 'react-typist';
 
 export default () => <div className="section">
     <div className="video-container">
@@ -8,11 +9,13 @@ export default () => <div className="section">
             Your browser does not support this video
         </video>
     </div>
-    
+
     <div className="content text-center">
-        <h1>Steven Liao</h1>
-        <h2>Front-End Developer</h2>
-        <p>HTML/CSS | JavaScript | BootStrap | ReactJS | NodeJS | MySQL ...</p>
+        <Typist>
+            <h1>Steven Liao</h1> 
+            <h2>Front-End Developer</h2>
+            <p>HTML/CSS | JavaScript | BootStrap | ReactJS | NodeJS | MySQL ...</p>
+        </Typist>
         <a href="#projects" className="btn btn-primary">My projects -></a>
     </div>
 </div>;
